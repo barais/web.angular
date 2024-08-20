4c1c849183a76f2b8b2c2bdb2dd3c73d931c069e
Olivier Barais <olivieR.barais@gmail.com>
2024-08-20 21:50:33 +0200

    update

---
+++ docs/assets/img13-ULg6HeM0.js
@@ -0,0 +1,1 @@
+const a="/web.angular/angular2/img13.png";export{a as _};
---
+++ docs/assets/slidev/DrawingPreview-CPpPBRvZ.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-ClNJMwwk.js";import"../modules/vue-DCbP7Xfw.js";import"../index-Cq63EqcP.js";import"../modules/shiki-Dv8q3HIQ.js";export{o as default};
